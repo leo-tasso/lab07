bcaf8c6ee929478b84221eb5becb869a915ebd0d
Tassinari Leonardo <leonardo.tassinari6@studio.unibo.it>
2022-11-08 18:47:14 +0100

    es 61 done, all test passed, not corrected

--- 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,8 +1,9 @@
 package it.unibo.nestedenum;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Locale;
-import java.util.Objects;
+import java.util.List;
 
 /**
  * Implementation of {@link MonthSorter}.
@@ -11,44 +12,66 @@ public final class MonthSorterNested implements MonthSorter {
 
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new Comparator<String>() {
+
+            @Override
+            public int compare(final String o1, String o2) {
+                return Month.fromString(o1).days - Month.fromString(o2).days;
+            }
+
+        };
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                return Month.fromString(o1).ordinal() - Month.fromString(o2).ordinal();
+            }
+        };
     }
-    
+
     private enum Month {
-        JAN("january",31),
-        FEB("feburary",28),
-        MAR("march",31),
-        APR("april",30),
-        MAY("may",31),
-        Jun("june",30),
-        Jul("july",31),
-        Aug("august",31),   
-        Sep("september",30),
-        Oct("october",31),
-        Nov("november",30),
-        Dec("december",31);
+        JAN("january", 31),
+        FEB("february", 28),
+        MAR("march", 31),
+        APR("april", 30),
+        MAY("may", 31),
+        Jun("june", 30),
+        Jul("july", 31),
+        Aug("august", 31),
+        Sep("september", 30),
+        Oct("october", 31),
+        Nov("november", 30),
+        Dec("december", 31);
 
+        private final String name;
+        private final int days;
 
-    private final String name;
-    private final int days;
-    private String getName(){
-        return this.getName(m);
-    }
-    private Month(final String name, int days) {
-        this.name = name;
-        this.days = days;
-    }
-    public Month fromString(String in){
-                for(Month m: Month.values()){
-                    if Month.getName(m).startsWith(in);
+        private String getName() {
+            return this.name;
+        }
+
+        private Month(final String name, int days) {
+            this.name = name;
+            this.days = days;
+        }
+
+        public static Month fromString(String in) {
+            List<Month> res = new ArrayList<>();
+            for (Month m : Month.values()) {
+                if (m.getName().startsWith(in.toLowerCase())) {
+                    res.add(m);
                 }
-    }
-    
+            }
+            if (res.size() == 1) {
+                return res.get(0);
+            } else {
+                throw new InvalidParameterException();
+            }
+        }
 
     }
 }
